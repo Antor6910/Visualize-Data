1e1d8e155af6fc0d201ce41260ef8463d8760939
antor-das <antor.csecu@gmail.com>
2024-09-09 20:37:56 +0600

    data visualize test file

---
+++ Matplotlib/straight_line.py
@@ -0,0 +1,13 @@
+form matplotlib import as plt
+
+#x values
+x=[1,2,3]
+
+#y values
+y=[4,5,6]
+
+#plot the graph by using plt.plot()
+plt.plot(x,y)
+
+#Display the graph by using plt.show()
+plt.show()
